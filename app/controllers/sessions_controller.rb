54601832121cf325d4376381201ef1ab8188b629
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-12 03:06:56 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def create
     if params[:name].present?
       session[:name] = params[:name]
+      render :welcome
     else
       redirect_to new_session_path
     end
