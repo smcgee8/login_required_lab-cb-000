f70f21ed4d0a63f9b67f6bc79537233d38507a24
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-12 03:10:01 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     if params[:name].present?
       session[:name] = params[:name]
-      redirect_to session_path
+      redirect_to session_path(nil)
     else
       redirect_to new_session_path
     end
